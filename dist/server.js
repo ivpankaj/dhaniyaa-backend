455f72cdb99db983bebe09770d8dc9b4a44e1d97
ivpankaj <imvpankaj@gmail.com>
2026-02-13 21:27:15 +0530

    zc

--- dist/server.js
+++ dist/server.js
@@ -7,7 +7,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
 const database_1 = require("./config/database");
 dotenv_1.default.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 (0, database_1.connectDatabase)();
 const socket_1 = require("./socket");
 const server = app_1.default.listen(PORT, () => {
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { connectDatabase } from './config/database';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 connectDatabase();
 
